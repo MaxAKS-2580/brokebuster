1027a67cab2bba337504ff6c490212a4e67e24e9
Akshat Abhishek Singh <136919042+MaxAKS-2580@users.noreply.github.com>
2025-08-04 01:57:13 +0530

    Delete django_backend directory

--- django_backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ping/', views.ping_view, name='ping'),
-    path('demo/', views.demo_view, name='demo'),
-    path('health/', views.health_check, name='health'),
-]
